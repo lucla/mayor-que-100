78b1ca6049af6f39ada969b28f4f0779d343390c
hugo <hugo.develop@gmail.com>
2016-11-18 16:09:28 +0100

    Add files via upload

---
+++ mayorQue100/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mayorQue100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool numeroOK = false;
+            double var1, var2;
+            var1 = 0;
+            var2 = 0;
+
+            while (!numeroOK)
+            {
+                Console.WriteLine("Escribe SOLO un número mayor que 100.");
+                Console.WriteLine("Escribe el primer número:");
+                var1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Escribe el segundo número:");
+                var2 = Convert.ToDouble(Console.ReadLine());
+                if (((var1 > 100) && (var2 > 100)) ^ ((var1 <= 100) && (var2 <= 100)))
+                {
+                    numeroOK = true;
+                    Console.WriteLine("Solo quiero un número mayor que 100.");
+                }
+                else
+                {
+                    Console.WriteLine($"Tus números son {var1} y {var2}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+            
